20d0083d15e3c967c3fe01c5bb56db69dcde80e7
Abdullah Solahudeen <solahudeen15@gmail.com>
2022-10-07 10:57:52 +0100

    db update created error 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,24 @@
+import { Box, Center, Heading, VStack } from "@chakra-ui/react"
+import { NextPage } from "next"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+const NotFound: NextPage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 3000);
+  }, [])
+  
+
+  return ( 
+    <VStack my={8} px={4}>
+      <Heading as='h2' size='xl' textAlign={'center'}>Salam, This page does not exist</Heading>
+      <Heading as='h4' size='md' textAlign={'center'}>Redirecting to homepage...</Heading>
+    </VStack>
+   );
+}
+ 
+export default NotFound;
--- pages/dua/[id].tsx
+++ pages/dua/[id].tsx
@@ -2,45 +2,47 @@ import { Box, Divider, Heading, Text, VStack } from "@chakra-ui/react"
 import { NextPage } from "next"
 
 export const getStaticPaths = async () => {
-    const res = await fetch('http://localhost:8000/dua/');
-    const data = await res.json();
-  
+    const res = await fetch("http://localhost:8000/dua/")
+    const data = await res.json()
+
     // map data to an array of path objects with params (id)
     const paths = data.map((ninja: { id: { toString: () => any } }) => {
-      return {
-        params: { id: ninja.id.toString() }
-      }
+        return {
+            params: { id: ninja.id.toString() },
+        }
     })
-  
+
     return {
-      paths,
-      fallback: false
+        paths,
+        fallback: false,
     }
-  }
-  
-  export const getStaticProps = async (context: { params: { id: any } }) => {
-    const id = context.params.id;
-    const res = await fetch('http://localhost:8000/dua/' + id);
-    const data = await res.json();
-  
+}
+
+export const getStaticProps = async (context: { params: { id: any } }) => {
+    const id = context.params.id
+    const res = await fetch("http://localhost:8000/dua/" + id)
+    const data = await res.json()
+
     return {
-      props: { duaList: data.dua, duaName: data.title }
+        props: { duaList: data.dua, duaName: data.title },
     }
-  }
-  type Props = {
+}
+type Props = {
     duaList: [
         item: {
-            arabic: string,
-            transliteration: string,
+            arabic: string
+            transliteration: string
             translation: string
-        },
-    ],
-    duaName: string,
-  }
-  const DuaList: NextPage<Props> = ({ duaList, duaName }) => {
+        }
+    ]
+    duaName: string
+}
+const DuaList: NextPage<Props> = ({ duaList, duaName }) => {
     return (
         <Box px={4} py={12}>
-            <Heading as='h2' textAlign='center' my={8}>{duaName}</Heading>
+            <Heading as="h2" size='lg' textAlign="center" my={8}>
+                {duaName}
+            </Heading>
             <VStack maxW={["xl"]} mx="auto" spacing={6}>
                 {duaList.map((item, index) => (
                     <VStack py={4} px={4} spacing={4} key={index}>
@@ -54,18 +56,14 @@ export const getStaticPaths = async () => {
                         >
                             {item.arabic}
                         </Box>
-                        <Text>
-                            {item.transliteration}
-                        </Text>
-                        <Divider />
-                        <Text>
-                            {item.translation}
-                        </Text>
+                        <Text>{item.transliteration}</Text>
+                        <Divider borderWidth={1} borderColor='blackAlpha.600'/>
+                        <Text>{item.translation}</Text>
                     </VStack>
                 ))}
             </VStack>
         </Box>
-    );
-  }
-  
-  export default DuaList;
+    )
+}
+
+export default DuaList
